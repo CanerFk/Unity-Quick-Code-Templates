4d935498abd23c376b57ed5b052d78fb436c25bb
Caner Furkan Karali <canerfkarali@gmail.com>
2023-08-27 15:06:57 +0300

    added crouch system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Camera playerCamera;
     [SerializeField] Transform groundCheckTransform;
     [SerializeField] LayerMask groundLayer;
-    public float mouseSensitivity { get; private set; } = 175f;
+    public float mouseSensitivity { get; private set; } = 100f;
     public float xRotation { get; private set; } = 0;
     public bool isGrounded { get; private set; } = false;
     public bool isWalking { get; private set; } = false;
     public bool isJumping { get; private set; } = false;
+    public bool isCrouching { get; private set; } = false;
+
     [System.NonSerialized] public Vector3 jumpVelocity = Vector3.zero;
 
     private CharacterController characterController;
@@ -35,7 +37,20 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         isWalking = Input.GetKey(KeyCode.LeftShift);
         Vector3 movementVector = Vector3.ClampMagnitude(verticalInput * transform.forward + horizontalInput * transform.right, 1.0f);
-        if(isWalking)
+        isCrouching = Input.GetKey(KeyCode.LeftControl);
+        
+        if(isCrouching)
+        {
+            HandleCrouch();
+        } else
+        {
+            HandleStand();
+        }
+
+        if (isCrouching)
+        {
+            characterController.Move(movementVector * playerStats.crouchMovementSpeed * Time.deltaTime);
+        }else if (isWalking)
         {
             characterController.Move(movementVector * playerStats.walkingMovementSpeed * Time.deltaTime);
         }
@@ -47,8 +62,8 @@ public class PlayerController : MonoBehaviour
     }
     void HandleMouseLook()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime * 2;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime * 2;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
@@ -77,5 +92,53 @@ public class PlayerController : MonoBehaviour
         }
         jumpVelocity.y += playerStats.gravity * Time.deltaTime;
         characterController.Move(jumpVelocity * Time.deltaTime);
+    }
+    void HandleCrouch()
+    {
+        if (characterController.height > playerStats.crouchHeightY)
+        {
+            UpdateCharacterHeight(playerStats.crouchHeightY);
+
+            if (characterController.height - 0.05f <= playerStats.crouchHeightY)
+            {
+                characterController.height = playerStats.crouchHeightY;
+            }
+        }
+    }
+    void HandleStand()
+    {
+        if(characterController.height < playerStats.standingHeightY)
+        {
+            float lastHeight = characterController.height;
+
+            //Checking for the presence of an object that will prevent the player from standing up
+            RaycastHit hit; 
+            if (Physics.Raycast(transform.position,transform.up,out hit, playerStats.standingHeightY))
+            {
+                if(hit.distance < playerStats.standingHeightY - playerStats.crouchHeightY)
+                {
+                    UpdateCharacterHeight(playerStats.crouchHeightY + hit.distance);
+                    return;
+                }
+                else
+                {
+                    UpdateCharacterHeight(playerStats.standingHeightY);
+                }
+            }
+            else
+            {
+                UpdateCharacterHeight(playerStats.standingHeightY);
+            }
+
+            if(characterController.height + 0.05f >= playerStats.standingHeightY)
+            {
+                characterController.height = playerStats.standingHeightY;
+            }
+            transform.position += new Vector3(0, (characterController.height-lastHeight/2),0);
+        }
+    }
+    void UpdateCharacterHeight(float newHeight)
+    {
+        characterController.height = Mathf.Lerp(characterController.height, newHeight, playerStats.crouchSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,7 +6,12 @@ public class PlayerStats : MonoBehaviour
 {
     public float runningMovementSpeed { get; private set; } = 6f;
     public float walkingMovementSpeed { get; private set; } = 3f;
+    public float crouchMovementSpeed { get; private set; } = 2f;
     public float gravity { get; private set; } = -21f;
     public float jumpHeight { get; private set; } = 1.5f;
+
+    public float standingHeightY { get; private set; } = 2f;
+    public float crouchHeightY { get; private set; } = 0.5f;
+    public float crouchSpeed { get; private set; } = 10f;
 
 }
